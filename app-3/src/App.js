9729a8af459c2f517886a2e496474a823f1823f4
ColbyBirrell <colby.birrell@gmail.com>
2020-01-29 15:04:20 -0700

    finished 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputLine: '',
+      array: ['sushi', 'seafood', 'eggs', 'pizza', 'cake', 'wings']
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+  handleChange(val) {
+    this.setState({ inputLine: val })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  render() {
+    let found = this.state.array.filter((element) => {
+      return element.includes(this.state.inputLine)
+    })
+
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>
+      })
+
+
+
+    return (
+      <div className="App">
+        <input onChange={(event) => this.handleChange(event.target.value)} />
+        <h2 className='h2'>{found}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;

07f5a0545e67c705709e1edf69a06d4982a091e1
ColbyBirrell <colby.birrell@gmail.com>
2020-01-29 16:01:56 -0700

    finished 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Image from './components/Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: <img src='https://i.kym-cdn.com/photos/images/original/000/027/661/HOERS.jpg' alt='elephant' />
+
+    }
+  }
+
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <Image imagePic={this.state.image} />
+
+      </div >
+    );
+  }
 }
+
+
 
 export default App;
---
+++ app-5/src/components/Image.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (this.props.imagePic)
+    }
+
+}
+
+export default Image;
